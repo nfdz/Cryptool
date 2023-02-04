5e03b26e8b4e47fbedcc283e991b9c73e4bf5099
nfdz <nfdz@proton.me>
2023-02-04 17:41:37 +0100

    WIP

---
+++ androidUI/src/androidTest/java/io/github/nfdz/cryptool/ui/encryption/LanSourceDialogScreenshotTest.kt
@@ -0,0 +1,39 @@
+package io.github.nfdz.cryptool.ui.encryption
+
+import dev.testify.ComposableScreenshotRule
+import dev.testify.annotation.ScreenshotInstrumentation
+import io.github.nfdz.cryptool.ui.DarkColorScheme
+import io.github.nfdz.cryptool.ui.LightColorScheme
+import io.github.nfdz.cryptool.ui.test.TestEntry
+import org.junit.Rule
+import org.junit.Test
+
+class LanSourceDialogScreenshotTest {
+
+    private val networkAddresses = listOf("1.2.3.4")
+    private val serverPort = 123
+    private val serverSlot = 3
+
+    @get:Rule
+    val rule = ComposableScreenshotRule()
+
+    @ScreenshotInstrumentation
+    @Test
+    fun light() {
+        rule.setCompose {
+            TestEntry(colorScheme = LightColorScheme) {
+                LanSourceDialogContent(networkAddresses, serverPort, serverSlot) {}
+            }
+        }.assertSame()
+    }
+
+    @ScreenshotInstrumentation
+    @Test
+    fun dark() {
+        rule.setCompose {
+            TestEntry(colorScheme = DarkColorScheme) {
+                LanSourceDialogContent(networkAddresses, serverPort, serverSlot) {}
+            }
+        }.assertSame()
+    }
+}
